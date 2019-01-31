92d75413d80d49246424f84371b5121b5fbde3fc
Morgan <brighi.morgan@wanadoo.fr>
2019-01-31 16:35:10 +0100

    Creation des differents composants + travail css

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,15 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { MissionsComponent } from './missions/missions.component';
+import { MissionDetailComponent } from './mission-detail/mission-detail.component';
+import { LoginComponent } from './login/login.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import {FormsModule} from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AccueilComponent
+    AccueilComponent,
+    MissionsComponent,
+    MissionDetailComponent,
+    LoginComponent,
+    InscriptionComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
